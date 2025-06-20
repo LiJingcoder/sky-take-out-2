b92991a3b85eb11f03d822686c7b6de3b8b4bd6d
LiJingcoder <2196573313@qq.com>
2025-06-20 16:02:27 +0800

    分类管理业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,67 @@
+package com.sky.controller.admin;
+
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 分类管理
+ */
+@RestController
+@Slf4j
+@RequestMapping("/admin/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/page")
+    @ApiOperation("分类分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        log.info("分类分页查询，参数为：{}",categoryPageQueryDTO);
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @PostMapping
+    @ApiOperation("新增分类")
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        log.info("新增分类：{}",categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    @PutMapping
+    @ApiOperation("修改分类信息")
+    //这里前端界面设置有问题
+    public Result update(@RequestBody CategoryDTO categoryDTO){
+        log.info("修改员工信息为：{}",categoryDTO);
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用、禁用分类")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用、禁用分类：{},{}",status,id);
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    @DeleteMapping
+    @ApiOperation("根据id删除分类")
+    //这部分内容涉及其他两个类的管理待修改
+
+    public Result deleteById(Long id){
+        categoryService.deleteById(id);
+        return Result.success();
+    }
+}
