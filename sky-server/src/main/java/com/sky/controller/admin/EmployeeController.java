5c8915c2b8d35f0ba567833dfb62675e8a919ee5
LiJingcoder <2196573313@qq.com>
2025-06-20 15:53:53 +0800

    分类管理业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -39,6 +39,7 @@ public class EmployeeController {
      * @return
      */
     @PostMapping("/login")
+    @ApiOperation("员工登录")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录：{}", employeeLoginDTO);
 
@@ -95,7 +96,7 @@ public class EmployeeController {
      */
     @GetMapping("/page")
     @ApiOperation("分页查询")
-    //这里参数不是json格式不用加@RequestBody，springMVC架构能直接识别这个参数
+    //只有Json格式才用@RequestBody修饰，这里是Query参数不是json格式不用加@RequestBody，springMVC架构能直接识别这个参数
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
         log.info("员工分页查询，参数为：{}",employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
@@ -111,7 +112,7 @@ public class EmployeeController {
     @PostMapping("/status/{status}")//{}表示路径参数
     @ApiOperation("启用禁用员工账号")
     public Result startOrStop(@PathVariable Integer status,Long id){//@PathVariable("status") Integer status,同名时可以省略括号内的参数
-        log.info("启用禁用员工账户",status,id);
+        log.info("启用禁用员工账户：{},{}",status,id);
         employeeService.startOrStop(status,id);
         return Result.success();
     }
@@ -121,6 +122,7 @@ public class EmployeeController {
      * @param id
      * @return
      */
+    //???既然分页查询那里前端已经拿到信息了，为什么还要用这步操作查询后再回显信息???
     @GetMapping("/{id}")
     @ApiOperation("根据id查询员工")
     public Result<Employee> getById(@PathVariable Integer id){
---
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -0,0 +1,41 @@
+package com.sky.service;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+
+public interface CategoryService {
+
+    /**
+     * 分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 新增分类信息
+     * @param categoryDTO
+     */
+    void save(CategoryDTO categoryDTO);
+
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     */
+    void update(CategoryDTO categoryDTO);
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    void deleteById(Long id);
+}
